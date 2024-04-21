b694dca942312f10cfcd50f689c7e68c03188048
YOmprakash <omprakashyamavaram@gmail.com>
2024-04-21 12:23:23 +0530

    header modified

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,31 +1,47 @@
-import {useState} from 'react'
-import { IoMdMenu } from "react-icons/io";
-import './index.css'
+import { useState } from 'react';
+import { IoMdMenu } from 'react-icons/io';
+import './index.css';
 
 function Navbar() {
-    const[menu,setMenu] = useState(false)
-    
+    const [isOpen, setOpen] = useState(false);
 
-  return (
-    <header>
-    <div>
-        <img src='https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/Nxtwave_Colored.svg' alt='logo' className='logo'/>
-       
+    const toggleMenu = () => {
+        setOpen(!isOpen);
+    }
 
-
-        <ul>
-            <a href='#'>Home</a>
-           <a href='#'>Programs</a>
-            <a href='#'>Reviews</a>
-           <a href='#'>Hire With Us</a>
-          
-            <a href='#login'>Login</a>
-           
-        </ul>
-
-    </div>      
-    </header>
-  )
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-brand">
+                    <a href="/"> <img src='https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/Nxtwave_Colored.svg' alt='logo' className='logo' />
+                    </a>
+                </div>
+                <div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
+                    <ul className="navbar-links">
+                        <li>
+                            <a href="#home" onClick={() => setOpen(false)}>Home</a>
+                        </li>
+                        <li>
+                            <a href="#about" onClick={() => setOpen(false)}>About</a>
+                        </li>
+                        <li>
+                            <a href="#services" onClick={() => setOpen(false)}>Services</a>
+                        </li>
+                        <li>
+                            <a href="#contact" onClick={() => setOpen(false)}>Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <button
+                    type="button"
+                    className="navbar-toggle"
+                    onClick={toggleMenu}
+                >
+                    <IoMdMenu color='#000' size={24} />
+                </button>
+            </div>
+        </nav>
+    )
 }
 
-export default Navbar
+export default Navbar;
